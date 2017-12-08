f53027a7f72c1851966b2fb0fba5c02db36599a5
Timwi <github@timwi.de>
2017-12-08 04:18:37 +0100

    Restructure directories to make the repo complete and self-contained.

---
+++ mk.bat
@@ -0,0 +1,8 @@
+@echo off
+mklink /j build "D:\Steam\steamapps\common\Keep Talking and Nobody Explodes\mods"
+mklink /j Assets\TestHarness D:\c\ktanemodkit\Assets\TestHarness
+mklink /j Assets\Editor\Scripts D:\c\ktanemodkit\Assets\Editor\Scripts
+mklink /j Assets\Editor\Steamworks.NET D:\c\ktanemodkit\Assets\Editor\Steamworks.NET
+mklink /j Assets\Plugins D:\c\ktanemodkit\Assets\Plugins
+mklink /j Assets\Shaders D:\c\ktanemodkit\Assets\Examples\Assets\Shaders
+mklink /j ProjectSettings D:\c\ktanemodkit\ProjectSettings
